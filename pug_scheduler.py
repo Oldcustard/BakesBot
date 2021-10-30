6ec1972283a943d732680b7da02bad908e2f698e
Oldcustard <147gusto@gmail.com>
2021-10-31 00:48:51 +1100

    fix line endings

--- pug_scheduler.py
+++ pug_scheduler.py
@@ -33,12 +33,15 @@ async def schedule_announcement(announce_channel: discord.TextChannel):
         announce_day = time.strptime(announce_wday, "%A")
         current_date = datetime.datetime.now(datetime.timezone.utc).astimezone()
         current_day = current_date.weekday()
-        time_to_announce = datetime.timedelta(days=announce_day.tm_wday - current_day, hours=int(announce_hour) - current_date.hour, minutes=int(announce_minute) - current_date.minute)
+        time_to_announce = datetime.timedelta(days=announce_day.tm_wday - current_day,
+                                              hours=int(announce_hour) - current_date.hour,
+                                              minutes=int(announce_minute) - current_date.minute)
         if time_to_announce.total_seconds() < 0:
             time_to_announce = datetime.timedelta(
-                days=time_to_announce.days + 8)  # Ensure announcement is in the future
+                days=time_to_announce.days + 7)  # Ensure announcement is in the future
         announce_date = current_date + time_to_announce
-        announce_date = announce_date.replace(hour=int(announce_hour), minute=int(announce_minute), second=0, microsecond=0)
+        announce_date = announce_date.replace(hour=int(announce_hour), minute=int(announce_minute), second=0,
+                                              microsecond=0)
         print(f"Pug announcement scheduled for {announce_date}")
         await messages.send_to_admin(f"{messages.host_role.mention}: Pug announcement scheduled for {datetime.datetime.strftime(announce_date, '%A (%d %B) at %X')}")
         await asyncio.sleep(seconds_until(announce_date))
