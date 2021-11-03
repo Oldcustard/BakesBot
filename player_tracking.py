b878c20dfca5ed9605768e81eea0c30f5ad83819
Oldcustard <147gusto@gmail.com>
2021-11-04 02:06:17 +1100

    define add_medic function and setup sqlite database

---
+++ player_tracking.py
@@ -0,0 +1,18 @@
+import sqlite3
+import discord
+
+
+def add_medic(player: discord.User):
+    player_name = player.name
+    db = sqlite3.connect('players.db')
+    c = db.cursor()
+
+    c.execute('''CREATE TABLE IF NOT EXISTS medics
+    (player TEXT PRIMARY KEY, weeks_remaining INTEGER)''')
+
+    c.execute('''SELECT player, weeks_remaining FROM medics WHERE player = ?''', (player_name,))
+    if c.fetchone() is None:
+        c.execute('''INSERT INTO medics (player, weeks_remaining)
+        VALUES (?, ?)''', (player_name, 3))
+
+    db.commit()
