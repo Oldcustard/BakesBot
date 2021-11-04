c2dd129574125b77fa1f1c7f81e5811b9f593d6b
dibbydoda <dibbydoda@gmail.com>
2021-11-04 17:22:53 +1100

    Capitalise more constants

--- pug_scheduler.py
+++ pug_scheduler.py
@@ -15,9 +15,9 @@ config = config['Pug Settings']
 
 pug_enabled = bool(strtobool(config['pug enabled']))
 
-announce_wday = config['announce weekday']
-announce_hour = config['announce hour']
-announce_minute = config['announce minute']
+ANNOUNCE_WDAY = config['announce weekday']
+ANNOUNCE_HOUR = config['announce hour']
+ANNOUNCE_MINUTE = config['announce minute']
 
 pugMessage: discord.Message
 
@@ -30,16 +30,16 @@ def seconds_until(desired_time: datetime.datetime):
 
 async def schedule_announcement(announce_channel: discord.TextChannel):
     while pug_enabled:
-        announce_day = time.strptime(announce_wday, "%A")
+        announce_day = time.strptime(ANNOUNCE_WDAY, "%A")
         current_date = datetime.datetime.now(datetime.timezone.utc).astimezone()
         current_day = current_date.weekday()
         time_to_announce = datetime.timedelta(days=announce_day.tm_wday - current_day,
-                                              hours=int(announce_hour) - current_date.hour,
-                                              minutes=int(announce_minute) - current_date.minute)
+                                              hours=int(ANNOUNCE_HOUR) - current_date.hour,
+                                              minutes=int(ANNOUNCE_MINUTE) - current_date.minute)
         if time_to_announce.total_seconds() < 0:
             time_to_announce = time_to_announce + datetime.timedelta(days=7)  # Ensure announcement is in the future
         announce_date = current_date + time_to_announce
-        announce_date = announce_date.replace(hour=int(announce_hour), minute=int(announce_minute), second=0,
+        announce_date = announce_date.replace(hour=int(ANNOUNCE_HOUR), minute=int(ANNOUNCE_MINUTE), second=0,
                                               microsecond=0)
         print(f"Pug announcement scheduled for {announce_date}")
         await messages.send_to_admin(f"{messages.dev.mention}: Pug announcement scheduled for {datetime.datetime.strftime(announce_date, '%A (%d %B) at %X')}")
