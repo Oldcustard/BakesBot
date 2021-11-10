d0137266be5df13e6a48051cb6d648551219b9a4
dibbydoda <dibbydoda@gmail.com>
2021-11-10 23:59:29 +1100

    Add messages for admin feedback

--- player_tracking.py
+++ player_tracking.py
@@ -64,7 +64,7 @@ async def update_early_signups():
             await member.remove_roles(messages.medic_role)  # Remove medic role from players not in the medic table
 
 
-async def warn_player(player: discord.User):
+async def warn_player(ctx: discord.ext.commands.Context, player: discord.User):
     player_name = player.name
     db = sqlite3.connect('players.db')
     c = db.cursor()
@@ -74,15 +74,18 @@ async def warn_player(player: discord.User):
 
     c.execute('''SELECT player, currently_warned FROM warnings WHERE player = ?''', (player_name,))
     row = c.fetchone()
+
     if row is None:  # Player is not on the warnings table, add them with 1 active warning
         c.execute('''INSERT INTO warnings (player, currently_warned, total_warnings)
          VALUES (?, ?, ?)''', (player_name, 1, 1))
+        await ctx.channel.send(f"{player_name} has been warned. {player_name} has 1 total warnings.")
     elif row[1]: # Player is on the warnings table, and has already been warned for this pug
-        pass
+        await ctx.channel.send(f"{player_name} has already been warned for this pug, no warning added. {player_name} has {row[2]} total warnings")
     else:  # Player is already on the warnings table, give them a current warning and add 1 to their total
         c.execute('''UPDATE warnings
          SET currently_warned = 1, total_warnings = total_warnings + 1
          WHERE player = ?''', (player_name,))
+        await ctx.channel.send(f"{player_name} has been warned. {player_name} has {row[2]} total warnings")
 
     db.commit()
     db.close()
