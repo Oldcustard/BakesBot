c1a06844d3d6163f5d3d9619f3bae1598425e507
Oldcustard <147gusto@gmail.com>
2021-11-15 15:34:28 +1100

    add string and reminder posting (#26)

--- player_selection.py
+++ player_selection.py
@@ -33,6 +33,8 @@ blu_name = ['blu', 'blue']
 
 bluMessage: discord.Message = None
 redMessage: discord.Message = None
+stringMessage: discord.Message = None
+reminderMessage: discord.Message = None
 
 
 async def select_player(ctx: discord.ext.commands.Context, team: str, player_class: str, player_obj: discord.Member):
@@ -53,6 +55,7 @@ async def select_player(ctx: discord.ext.commands.Context, team: str, player_cla
             await bluMessage.pin()
         else:
             await bluMessage.edit(content="BLU Team:\n" + await list_players(blu_team))
+            await announce_string()
 
     elif team.lower() == 'red':
         red_team[player_class.capitalize()] = player_obj
@@ -64,6 +67,7 @@ async def select_player(ctx: discord.ext.commands.Context, team: str, player_cla
             await bluMessage.pin()
         else:
             await redMessage.edit(content="RED Team:\n" + await list_players(red_team))
+            await announce_string()
     else:
         await ctx.channel.send("Team not recognised")
         return
@@ -80,3 +84,17 @@ async def list_players(team: Dict):
             line = Class + ": " + player.mention
         msg = msg + "\n" + line
     return msg
+
+
+async def announce_string(connect_string=None):
+    global stringMessage, reminderMessage
+    msg = f"{bluMessage.content}\n\n{redMessage.content}"
+    if connect_string is None:  # Function was called to update players
+        if reminderMessage is not None:  # Check if reminder message already exists
+            await reminderMessage.edit(content=msg)
+        return
+    if stringMessage is None:  # First string
+        stringMessage = await bluMessage.channel.send(connect_string)
+        reminderMessage = await bluMessage.channel.send(msg)
+    else:  # Updated string
+        await stringMessage.edit(content=connect_string)
