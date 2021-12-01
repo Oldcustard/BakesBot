18359bd7450df57f7a743c0fb5a7c4aa2deac50a
Oldcustard <147gusto@gmail.com>
2021-12-01 15:46:13 +1100

    fix winning team check to be BLU not BLUE

--- elo_tracking.py
+++ elo_tracking.py
@@ -111,7 +111,7 @@ async def update_elo(winning_team, round_wins, game_type):
                     c.execute(f'''UPDATE elo 
                     SET {player_class} = {player_class} + ? WHERE player_id = ?''', (elo_change, player.id))
     elif game_type == 'AD':
-        if winning_team == 'BLUE':
+        if winning_team == 'BLU':
             exp_result = elo.expected_result("BluAVG", "RedAVG", names=True)
             elo_change = round(elo.k * (1 - exp_result))
             for player_class, player in player_selection.blu_team.items():
