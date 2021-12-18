51f72d3abac830ae65cda4dcee1ee31d229eeecc
Oldcustard <147gusto@gmail.com>
2021-12-18 21:18:32 +1100

    Feature to remove votes from withdrawn users (#56)
    * fixes #52: add messages_to_delete list and try/except block
    
    * sync start_pug.py
    
    * include active votes and ping messages in the delete loop
    
    * add clear_user_votes function

--- map_voting.py
+++ map_voting.py
@@ -6,7 +6,7 @@ import active_pug
 import messages
 import player_selection
 
-active_votes: List[discord.Message] = []
+active_votes: List[discord.MessageReference] = []
 
 emoji_list = ('1️⃣', '2️⃣', '3️⃣', '4️⃣', '5️⃣', '6️⃣', '7️⃣', '8️⃣', '9️⃣', '🔟')
 
@@ -17,7 +17,7 @@ async def start_map_vote(ctx: discord.ext.commands.Context, *maps):
         line = f"{emoji_list[i]} {map_name.capitalize()}"
         map_list_string = map_list_string + "\n" + line
     message = await messages.announceChannel.send("Map voting open. Please select from the maps below\n" + map_list_string)
-    active_votes.append(message)
+    active_votes.append(discord.MessageReference.from_message(message))
     active_pug.start_pug.messages_to_delete.append(message)
     for i in range(len(maps)):
         await message.add_reaction(emoji_list[i])
@@ -29,3 +29,10 @@ async def vote_for_map(reaction: discord.Reaction, user: discord.Member):
         await reaction.remove(user)
         await user.send("You may only vote once you have been assigned to a class.")
         return
+
+
+async def clear_user_votes(user: discord.Member):
+    for vote_ref in active_votes:
+        vote = vote_ref.cached_message
+        for reaction in vote.reactions:
+            await reaction.remove(user)
