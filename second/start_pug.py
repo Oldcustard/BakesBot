7481c9036ad85a188c28d4ea8769c3abe5697fa4
Oldcustard <147gusto@gmail.com>
2022-01-08 11:27:00 +1100

    remove everyone ping

--- second/start_pug.py
+++ second/start_pug.py
@@ -68,7 +68,7 @@ async def announce_pug(channel: discord.TextChannel):
     print(f"Pug announced. Pug is on {pug_date}")
     pug_timestamp = round(datetime.datetime.timestamp(pug_date))
     pug_time_string = f"<t:{pug_timestamp}:F>"
-    announce_message = f"@everyone\n{ANNOUNCE_STRING} \nPug will be **{pug_time_string}** (this is displayed in your **local time**)\nPress withdraw if you can no longer play."
+    announce_message = f"\n{ANNOUNCE_STRING} \nPug will be **{pug_time_string}** (this is displayed in your **local time**)\nPress withdraw if you can no longer play."
     view = discord.ui.View(timeout=None)
     for class_name, class_emoji in emojis_ids.items():
         button = discord.ui.Button(label=class_name, emoji=class_emoji)
@@ -84,7 +84,7 @@ async def announce_pug(channel: discord.TextChannel):
 
 async def announce_early(early_signups_channel: discord.TextChannel, signups_channel: discord.TextChannel):
     announce_message = f"{messages.medic_role.mention}\n{EARLY_ANNOUNCE_STRING} \nPress withdraw if you can no longer play."
-    medic_announce_message = f"@everyone\nEarly signups open!\nIf you want to play **Medic**, press the button below. Medics will gain 3 weeks of early signup!"
+    medic_announce_message = f"\nEarly signups open!\nIf you want to play **Medic**, press the button below. Medics will gain 3 weeks of early signup!"
     early_view = discord.ui.View(timeout=None)
     for class_name, class_emoji in emojis_ids.items():
         button = discord.ui.Button(label=class_name, emoji=class_emoji)
