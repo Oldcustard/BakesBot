36d13b4c8a93d12945b84681612cc0d51fa44228
dibbydoda <dibbydoda@gmail.com>
2022-01-19 14:15:37 +1100

    Refactor 'msg = msg + x' to 'msg += x'

--- openskill_tracking.py
+++ openskill_tracking.py
@@ -183,32 +183,32 @@ async def get_team_balance(inter: discord.ApplicationCommandInteraction):
             line = f"\n{'**' if better == 'BLU' else ''}{player_selection.blu_team[player_class].display_name} ({player_ranks[0]}){'**' if better == 'BLU' else ''}" \
                    f" {start_pug.emojis_ids[player_class]}" \
                    f" {'**' if better == 'RED' else ''}{player_selection.red_team[player_class].display_name} ({player_ranks[1]}){'**' if better == 'RED' else ''}"
-            msg = msg + line
+            msg += line
         blu_avg = round(sum(blu_ranks)/len(blu_ranks), 2)
         red_avg = round(sum(red_ranks)/len(red_ranks), 2)
         team_better = 'BLU' if blu_avg > red_avg else 'RED'
-        msg = msg + f"\n{'**' if team_better == 'BLU' else ''}BLU Average: {blu_avg}{'**' if team_better == 'BLU' else ''} 🟦🟥 " \
+        msg += f"\n{'**' if team_better == 'BLU' else ''}BLU Average: {blu_avg}{'**' if team_better == 'BLU' else ''} 🟦🟥 " \
                     f"{'**' if team_better == 'RED' else ''}RED Average: {red_avg}{'**' if team_better == 'RED' else ''}"
 
-        msg = msg + f"\n\n**{start_pug.emojis_ids['Pyro']}{start_pug.emojis_ids['Demo']}Combo Balance{start_pug.emojis_ids['Heavy']}{start_pug.emojis_ids['Medic']}**"
+        msg += f"\n\n**{start_pug.emojis_ids['Pyro']}{start_pug.emojis_ids['Demo']}Combo Balance{start_pug.emojis_ids['Heavy']}{start_pug.emojis_ids['Medic']}**"
         blu_combo = round((blu_ranks[2] + blu_ranks[3] + blu_ranks[4] + blu_ranks[6])/4, 2)
         red_combo = round((red_ranks[2] + red_ranks[3] + red_ranks[4] + red_ranks[6])/4, 2)
         combo_better = 'BLU' if blu_combo > red_combo else 'RED'
-        msg = msg + f"\n{'**' if combo_better == 'BLU' else ''}BLU Average: {blu_combo}{'**' if combo_better == 'BLU' else ''} 🟦🟥 " \
+        msg += f"\n{'**' if combo_better == 'BLU' else ''}BLU Average: {blu_combo}{'**' if combo_better == 'BLU' else ''} 🟦🟥 " \
                     f"{'**' if combo_better == 'RED' else ''}RED Average: {red_combo}{'**' if combo_better == 'RED' else ''}"
 
-        msg = msg + f"\n\n**{start_pug.emojis_ids['Scout']}Flank Balance{start_pug.emojis_ids['Soldier']}**"
+        msg += f"\n\n**{start_pug.emojis_ids['Scout']}Flank Balance{start_pug.emojis_ids['Soldier']}**"
         blu_flank = round((blu_ranks[0] + blu_ranks[1]) / 2, 2)
         red_flank = round((red_ranks[0] + red_ranks[1]) / 2, 2)
         flank_better = 'BLU' if blu_flank > red_flank else 'RED'
-        msg = msg + f"\n{'**' if flank_better == 'BLU' else ''}BLU Average: {blu_flank}{'**' if flank_better == 'BLU' else ''} 🟦🟥 " \
+        msg += f"\n{'**' if flank_better == 'BLU' else ''}BLU Average: {blu_flank}{'**' if flank_better == 'BLU' else ''} 🟦🟥 " \
                     f"{'**' if flank_better == 'RED' else ''}RED Average: {red_flank}{'**' if flank_better == 'RED' else ''}"
 
-        msg = msg + f"\n\n**{start_pug.emojis_ids['Sniper']}Picks Balance{start_pug.emojis_ids['Spy']}**"
+        msg += f"\n\n**{start_pug.emojis_ids['Sniper']}Picks Balance{start_pug.emojis_ids['Spy']}**"
         blu_picks = round((blu_ranks[7] + blu_ranks[8]) / 2, 2)
         red_picks = round((red_ranks[7] + red_ranks[8]) / 2, 2)
         picks_better = 'BLU' if blu_picks > red_picks else 'RED'
-        msg = msg + f"\n{'**' if picks_better == 'BLU' else ''}BLU Average: {blu_picks}{'**' if picks_better == 'BLU' else ''} 🟦🟥 " \
+        msg += f"\n{'**' if picks_better == 'BLU' else ''}BLU Average: {blu_picks}{'**' if picks_better == 'BLU' else ''} 🟦🟥 " \
                     f"{'**' if picks_better == 'RED' else ''}RED Average: {red_picks}{'**' if picks_better == 'RED' else ''}"
         await inter.send(msg)
     finally:
